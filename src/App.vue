1769b9a0135953e358461c48112c3e46a3138265
Sergio Panjoj <sergiopanjoj@gmail.com>
2020-05-01 23:21:35 -0700

    Add more than one note to notebook

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,34 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <h1>Notebook</h1>
+    
+  <section id="main">
+  <!-- Main app -->
+      <div class="row no-gutters" style="height: 100%;">
+        <div class="col-2">
+          <div class="aside-wrapper">
+            <h3>Notebook</h3>
+            <button @click="addNote">Add Note</button>
+            <div>
+              <div v-for="note of notes" :key="note.title">
+                {{note.title}}
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-5">
+          <!-- Text input pane --> 
+          <section class="textarea-wrapper">
+          <textarea placeholder="Write here" v-model="content"></textarea>
+          </section>
+        </div>
+        <div class="col-5">
+          <!-- Preview pane -->
+          <section class="preview" v-html="notePreview">
+          </section>
+        </div>
 
-    <!-- Main pane --> 
-    <section class="main">
-      <textarea placeholder="Write here" v-model="content"></textarea>
+      </div>
     </section>
-
-    <!-- Preview pane -->
-    <aside class="preview" v-html="notePreview">
-    </aside>
   </div>
 </template>
 
@@ -19,6 +37,19 @@ import marked from "marked"
 
 export default {
   name: 'App',
+  methods: {
+    addNote() {
+      const time = Date.now()
+      const note = {
+        id: String(time),
+        title: "New Note",
+        content: "**Hi!**",
+        created: time,
+        favorite: false
+      }
+      this.notes.push(note)
+    }
+  },
   computed: {
     notePreview () {
       return marked(this.content)
@@ -30,11 +61,12 @@ export default {
     }
   },
   created() {
-    this.content = localStorage.getItem("content") || " "
+    this.content = localStorage.getItem("content") || ""
   },
   data () {
     return {
-      content: ""
+      content: "",
+      notes: []
     }
   }
 }
@@ -42,19 +74,44 @@ export default {
 
 <style>
 #app {
+  display: flex;
+  flex-flow: column;
+  height: 100vh;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 
-.main {
-  background-color: #eee;
+#main {
+  height: 100%;
+  overflow-y: scroll;
+}
+
+.textarea-wrapper {
+  width: 100%;
+  height: 100%;
+  padding: 20px;
+  background-color: aqua;
 }
 
 .preview {
   background-color: #ddd;
+  width: 100%;
+  height: 100%;
+  padding: 20px;
+  overflow-y: scroll;
+}
+
+textarea {
+  height: 100%;
+  width: 100%;
+  border: none;
+  resize: none !important;
+  outline: none;
+}
+
+.aside-wrapper {
+  padding: 10px;
 }
 </style>
